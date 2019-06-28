0df3bcd3baccbc9b9b37d4efc02763d38c1f8294
praveekrish <52271557+praveekrish@users.noreply.github.com>
2019-06-28 15:06:14 +0530

    Create natural.py

---
+++ natural.py
@@ -0,0 +1,8 @@
+# your code goes here
+n = input("Enter Number to calculate sum")
+n = int (n)
+average = 0
+sum = 0
+for num in range(0,n+1,1):
+    sum = sum+num
+print("SUM of first ", n, "numbers is: ", sum )
