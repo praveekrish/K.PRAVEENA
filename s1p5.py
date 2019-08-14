5ecaf24576b0360d343c01daf3dfa20b03b54248
praveekrish <52271557+praveekrish@users.noreply.github.com>
2019-08-14 14:09:10 +0530

    Update s1p5.py

--- s1p5.py
+++ s1p5.py
@@ -1,4 +1,4 @@
-#a
+#p
 list11=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
 list21=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 w=str(input())
